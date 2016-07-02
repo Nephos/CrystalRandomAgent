25ad31cd48249e6e04af2a1dd28096f17e480ed6
Arthur Poulet <arthur.poulet@mailoo.org>
2016-07-03 00:26:03 +0200

    fix format (crystal tool format)

--- spec/crystal_random_agent_spec.cr
+++ spec/crystal_random_agent_spec.cr
@@ -1,7 +1,6 @@
 require "./spec_helper"
 
 describe CrystalRandomAgent do
-
   it "works" do
     (CrystalRandomAgent::USER_AGENTS.size > 100).should eq true
     100.times do
@@ -9,5 +8,4 @@ describe CrystalRandomAgent do
       CrystalRandomAgent::USER_AGENTS.includes?(ua).should eq true
     end
   end
-
 end
--- src/crystal_random_agent.cr
+++ src/crystal_random_agent.cr
@@ -2,15 +2,14 @@ require "./crystal_random_agent/*"
 require "yaml"
 
 module CrystalRandomAgent
-
   CONFIG_FILE = File.expand_path "./user_agents.yml", __DIR__
-  FLAT_YAML = {{ system("echo '\"'; cat #{__DIR__}/user_agents.yml; echo '\"'") }}
-  CONFIG = YAML.parse FLAT_YAML
+  FLAT_YAML   = {{ system("echo '\"'; cat #{__DIR__}/user_agents.yml; echo '\"'") }}
+  CONFIG      = YAML.parse FLAT_YAML
 
   USER_AGENTS = CONFIG["user_agents"].as_h.map do |_, subtypes|
     YAML::Any.new(subtypes).as_h.map do |_, agents|
-       YAML::Any.new(agents).as_a.map{|ua| ua as String}
-     end
+      YAML::Any.new(agents).as_a.map { |ua| ua.as(String) }
+    end
   end.flatten
 
   def get_random_user_agent : String
@@ -18,5 +17,4 @@ module CrystalRandomAgent
   end
 
   extend self
-
 end
